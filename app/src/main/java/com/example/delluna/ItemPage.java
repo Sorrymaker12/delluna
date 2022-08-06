58f124edc1bc125b85fe5e000e429b1ab6db939c
goinesws <64315827+goinesws@users.noreply.github.com>
2022-08-07 02:51:37 +0700

    tab

--- app/src/main/java/com/example/delluna/ItemPage.java
+++ app/src/main/java/com/example/delluna/ItemPage.java
@@ -1,14 +1,34 @@
 package com.example.delluna;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class ItemPage extends AppCompatActivity {
+
+    TabLayout tlTab;
+    ViewPager2 vpFragment;
+
+    void init() {
+        tlTab = findViewById(R.id.tlTab);
+        vpFragment = findViewById(R.id.vpFragment);
+    }
+
+    void populateTab() {
+        tlTab.addTab(tlTab.newTab().setText("Cloth"));
+        tlTab.addTab(tlTab.newTab().setText("Album"));
+        tlTab.addTab(tlTab.newTab().setText("Other"));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_page);
+
+        init();
+        populateTab();
     }
 }
